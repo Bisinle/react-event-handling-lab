74d3fad2703d0c81e0a424062fcd5c0db4d9cf42
Bisinle <Abdiwadud.mohamedd@gmail.com>
2023-07-19 12:08:57 +0300

    passed all the tests

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,22 @@
 // Code EyesOnMe Component Here
+import React from "react";
+
+function EyesOnMe() {
+  function focusHandle(e) {
+    console.log("Good!");
+  }
+  function blurHandle(e) {
+    let button = e.target;
+    button.removeEventListener("focus", focusHandle);
+    console.log("Hey! Eyes on me!");
+  }
+  return (
+    <div>
+      <button onFocus={focusHandle} onBlur={blurHandle}>
+        Eyes on me
+      </button>
+    </div>
+  );
+}
+
+export default EyesOnMe;
